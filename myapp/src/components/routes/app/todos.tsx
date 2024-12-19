3fd6e3c04ae1c40c44f140b5d664a207b886e036
Aditya Meena <aditya.meena@iicstechnologies.com>
2024-12-19 14:25:33 +0530

    added filter and edit functionality for the post

--- myapp/src/components/routes/app/todos.tsx
+++ myapp/src/components/routes/app/todos.tsx
@@ -4,7 +4,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { gql, useMutation } from '@apollo/client'
 import { useAuthQuery } from '@nhost/react-apollo'
-import { Info, Plus, Trash } from 'lucide-react'
+import { Edit, Info, Plus, Trash } from 'lucide-react'
 import { useState } from 'react'
 import { toast } from 'sonner'
 
@@ -24,9 +24,16 @@ export default function Todos() {
       }
     }
   `)
-console.log('data',data)
   const [contents, setContents] = useState('')
   const [title, setTitle] = useState('')
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [fromDate, setFromDate] = useState<string>('')
+  const [toDate, setToDate] = useState<string>('')
+  const [selectedObj, setSelectedObj] = useState<{
+    id: string
+    title: string
+    content: string
+  } | null>(null)
 
   // Insert Post Mutation
   const [addPost] = useMutation<{
@@ -80,6 +87,27 @@ console.log('data',data)
     }
   `)
 
+  const [updatePost] = useMutation<{
+    update_Posts_by_pk?: {
+      id: string
+      title: string
+      content: string
+      author_id: string
+    }
+  }>(gql`
+   mutation UpdatePost($id: uuid!, $title: String!, $content: String!) {
+      update_Posts_by_pk(
+        pk_columns: { id: $id }
+        _set: { title: $title, content: $content }
+      ) {
+        id
+        title
+        content
+        author_id
+      }
+    }
+  `)
+
   const handleAddPost = () => {
     if (contents && title) {
       addPost({
@@ -97,7 +125,7 @@ console.log('data',data)
           toast.success('Post added successfully!')
         },
         onError: (error) => {
-          toast.error(error.message)
+          toast.error(error.message ? "You are not allowed to add this post" : "")
         }
       })
     } else {
@@ -113,10 +141,54 @@ console.log('data',data)
         toast.success('Post deleted successfully!')
       },
       onError: (error) => {
-        toast.error(error.message)
+        toast.error(error.message ? "You are not allowed to delete this post" : "")
       }
     })
   }
+
+  const handleEditPost = async () => {
+    await updatePost({
+      variables: { id: selectedObj?.id, title: title, content: contents },
+      onCompleted: async () => {
+        await refetchTodos()
+        toast.success('Post edited successfully!')
+        setIsModalOpen(false)
+        setTitle("")
+        setContents("")
+      },
+      onError: (error) => {
+        toast.error(error.message ? "You are not allowed to edit this post" : "")
+      }
+    })
+  }
+
+  const handleEditPostModal = (post: { id: string; title: string; content: string }) => {
+    setSelectedObj(post)
+    setTitle(post.title)
+    setContents(post.content)
+    setIsModalOpen(true)
+  }
+
+  const handleEditCancel = () => {
+    setIsModalOpen(false)
+    setTitle("")
+    setContents("")
+  }
+
+  // const handleDateFilter = () => {
+  //   if (fromDate && toDate) {
+  //     const filteredPosts = data?.Posts.filter((post) => {
+  //       const postDate = moment(post.created_at)
+  //       const from = moment(fromDate)
+  //       const to = moment(toDate)
+  //       return postDate.isBetween(from, to, 'day', '[]') // Inclusive of both dates
+  //     })
+  //     return filteredPosts || []
+  //   }
+  //   return data?.Posts || []
+  // }
+
+  // const filteredPosts = handleDateFilter()
 
   return (
     <div className="w-full">
@@ -144,6 +216,35 @@ console.log('data',data)
               Add
             </Button>
           </div>
+          <div className="border rounded-lg p-4">
+            <h3 className="text-lg font-semibold text-gray-900 mb-4">Filter</h3>
+            <div className="flex flex-row gap-4">
+              <div className="flex flex-col flex-3">
+                <label htmlFor="fromDate" className="text-sm font-medium text-gray-900">
+                  From
+                </label>
+                <Input
+                  id="fromDate"
+                  type="date"
+                  value={fromDate}
+                  onChange={(e) => { setFromDate(e.target.value); }}
+                  className="mt-1"
+                />
+              </div>
+              <div className="flex flex-col flex-3">
+                <label htmlFor="toDate" className="text-sm font-medium text-gray-900">
+                  To
+                </label>
+                <Input
+                  id="toDate"
+                  type="date"
+                  value={toDate}
+                  onChange={(e) => setToDate(e.target.value)}
+                  className="mt-1"
+                />
+              </div>
+            </div>
+          </div>
           <div>
             {data?.Posts.length === 0 && (
               <Alert className="w-full">
@@ -163,17 +264,48 @@ console.log('data',data)
                   <span className="font-bold">{post.title}</span>
                   <span>{post.content}</span>
                 </div>
-                <Button
-                  variant="ghost"
-                  onClick={() => handleDeletePost(post.id)}
-                >
-                  <Trash className="w-5 h-5" />
-                </Button>
+                <div>
+                  <Button
+                    variant="ghost"
+                    onClick={() => handleEditPostModal(post)}
+                  >
+                    <Edit className="w-5 h-5" />
+                  </Button>
+                  <Button
+                    variant="ghost"
+                    onClick={() => handleDeletePost(post.id)}
+                  >
+                    <Trash className="w-5 h-5" />
+                  </Button>
+                </div>
               </div>
             ))}
           </div>
         </CardContent>
       </Card>
+      {isModalOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white p-6 rounded-md w-96">
+            <h3 className="text-lg font-semibold mb-4">Edit Post</h3>
+            <Input
+              placeholder="Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className="mb-4"
+            />
+            <Input
+              placeholder="Content"
+              value={contents}
+              onChange={(e) => setContents(e.target.value)}
+              className="mb-4"
+            />
+            <div className="flex justify-end gap-2">
+              <Button onClick={() => handleEditCancel()}>Cancel</Button>
+              <Button onClick={() => handleEditPost()}>Add</Button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
